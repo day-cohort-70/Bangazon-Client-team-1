67529ccff9b45b77027840c2ed819f72d7503192
Niki Powell <nhe.powell@gmail.com>
2024-07-02 14:04:52 -0500

    updated properties so that both stores and profiles could access the same information from props

--- components/store/card.js
+++ components/store/card.js
@@ -11,7 +11,7 @@ export function StoreCard({ store, width= "is-half" }) {
         </header>
         <div className="card-content">
           <p className="content">
-            Owner: {store.seller.first_name} {store.seller.last_name}
+            Owner: {store.seller.user?.first_name} {store.seller.user?.last_name}
           </p>
           <div className="content">
             {store.description}
